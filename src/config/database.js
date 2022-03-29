2b100b7fdb8d8592edbb79185a828d2cfc78a51c
JP2021 <joaopaulo.atm@gmail.com>
2022-03-29 14:30:33 -0300

    configuração inicial

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports={
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password:'351465nt',
+    database:'backendsql',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
