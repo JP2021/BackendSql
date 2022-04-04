396ecba8f8bea3fd4fc8d0f9dac22e2e11e82665
JP2021 <joaopaulo.atm@gmail.com>
2022-04-04 15:43:10 -0300

    Cadastro de Endereço ok

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,10 +2,21 @@ const User = require('../models/User.js');
 
 module.exports={
 
+    async index(req, res){
+        const user = await User.findAll();
+         return res.json(user);
+    },
     async store(req,res){
 
-       
+      
         const {name,email} = req.body;
+
+        const userExists= await User.findOne({ where: {email: req.body.email}});
+
+        if (userExists) {
+    
+          return res.status(400).json({error: 'User already exists',})
+        }
 
         const user = await User.create({name,email});
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require ('express');
 const Usercontroller = require('./controllers/UserController');
+const Address = require('./controllers/AddressController');
+const AddressController = require('./controllers/AddressController');
 
 const routes = express.Router();
 
 routes.post('/users', Usercontroller.store);
+routes.get('/users',Usercontroller.index);
+routes.post('/users/:user_id/addresses', AddressController.store)
 
 
 module.exports = routes; 
