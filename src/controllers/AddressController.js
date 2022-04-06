ad8bee562d5da668dd06fa6ffff78ecb333c01e0
JP2021 <joaopaulo.atm@gmail.com>
2022-04-06 15:17:49 -0300

    Cadastro de Techs

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -1,26 +1,34 @@
-const Address= require('../models/Address');
 const User = require('../models/User');
+const Address = require('../models/Address');
 
-module.exports={
+module.exports = {
+  async index(req, res) {
+    const { user_id } = req.params;
 
-       async store(req,res){
+    const user = await User.findByPk(user_id, {
+      include: { association: 'addresses' }
+    });
 
-       const {user_id} = req.params;
-        const {zipcode, street,number} = req.body;
+    return res.json(user);
+  },
 
-        const user = await User.findByPk(user_id);
-        if (!user){
-            return res.status(400).json({error: 'User not found'});
-        }
+  async store(req, res) {
+    const { user_id } = req.params;
+    const { zipcode, street, number } = req.body;
 
-        const addresses = await Address.create({
-            zipcode,
-            street,
-            number,
-            user_id,
+    const user = await User.findByPk(user_id);
 
-        });
-
-        return res.json(addresses);
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
     }
+
+    const address = await Address.create({
+      zipcode,
+      street,
+      number,
+      user_id,
+    });
+
+    return res.json(address);
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const express = require ('express');
 const Usercontroller = require('./controllers/UserController');
-const Address = require('./controllers/AddressController');
 const AddressController = require('./controllers/AddressController');
+const TechController = require('./controllers/TechController');
 
 const routes = express.Router();
 
 routes.post('/users', Usercontroller.store);
 routes.get('/users',Usercontroller.index);
-routes.post('/users/:user_id/addresses', AddressController.store)
+routes.post('/users/:user_id/addresses', AddressController.store);
+routes.get('/users/:user_id/addresses', AddressController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.get('/users/:user_id/techs', TechController.index);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 
 module.exports = routes; 
